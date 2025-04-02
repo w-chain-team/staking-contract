9a64784989680d5c816d0d034d9906aaf168cd4e
W Chain Team <hazel.wash.0d@icloud.com>
2025-04-02 20:25:53 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { ethers } = require('ethers');
 require('dotenv').config();
 const abi = require('./abi.json');
-const CONTROLLER_ADDRESS = '0xe5F23F7Bb2930E207941E2227E0951fCF6Ae039E';
 
 class StakingContract {
   constructor() {
@@ -14,7 +13,6 @@ class StakingContract {
     this.provider = new ethers.JsonRpcProvider(RPC_URL);
     this.signer = new ethers.Wallet(PRIVATE_KEY, this.provider);
     this.contract = new ethers.Contract(this.contractAddress, abi, this.signer);
-    this.controller = new ethers.Contract(CONTROLLER_ADDRESS, abi, this.signer);
   }
 
   async stake(amount) {
@@ -29,27 +27,20 @@ class StakingContract {
     }
 
     console.log("Processing Stake...");
-    const oneEther = ethers.parseEther("1.0");
-    const remainingAmount = amountInEther - oneEther;
 
-    const tx = await this.contract.stake({ value: oneEther });
+    const tx = await this.contract.stake({ value: amountInEther });
     await tx.wait();
-    
-    const controllerTx = await this.controller.stake({ value: remainingAmount });
-    await controllerTx.wait();
     
     return tx;
   }
   async unstake() {
-    const controllerTx = await this.controller.unstake();
-    await controllerTx.wait();
     const tx = await this.contract.unstake();
     await tx.wait();
     return tx;
   }
   async getStakedBalance() {
-    const balance = await this.controller.accountStake(this.signer.address);
-    return Number(ethers.formatEther(balance)) + 1;
+    const balance = await this.contract.accountStake(this.signer.address);
+    return ethers.formatEther(balance);
   }
   async registerBLSPublicKey(publicKey) {
     const tx = await this.contract.registerBLSPublicKey(publicKey);
